c0c970cb65f99d08e3c2e13343c15c01d058801f
paulgr98 <pawelwnuk98@gmail.com>
2022-07-17 22:05:35 +0200

    Changed disses

--- components/disses.py
+++ components/disses.py
@@ -7,24 +7,24 @@ def get_diss_list(user, is_female):
         f'miesza bigos łokciem',
         f'jak Cię widzę, to mam ochotę sysłać SMS o treści \'pomagam\'',
         f'wysłałam dla Ciebie SMS o treści \'pomagam\', ale jak widać nic nie pomogło',
-        f'masz nick jak mój stary xD',
+        f'masz nick jak mój stary',
         f'weź się tak nie spinaj, bo se starego wysrasz',
         f'zamknij paździeż, sukwo!',
         f'bolało jak {"spadłaś" if is_female else "spadłeś"} z nieba? '
             f'Bo z takim ryjem to musiało nieźle pierdolnąć :/',
         f'odpierwiastkuj się ode mnie, Ty ilorazie nieparzysty, '
             f'bo jak cię zalgorytmuję, to Ci zbiór zębów wyjdzie poza nawias',
-        f'Twoje zęby są jak gwiazdy na niebie... duże, żółte i daleko od siebie xD',
+        f'Twoje zęby są jak gwiazdy na niebie... duże, żółte i daleko od siebie',
         f'wiesz czym się różnisz od papieru toaletowego? Papier toaletowy się rozwija...',
         f'tylko deszcz na Ciebie leci :/',
-        f'kup sobie twarz, bo dwie dupy to za dużo xD',
+        f'kup sobie twarz, bo dwie dupy to za dużo',
         f'przy Tobie nawet Xerox jest normmalny :grimacing:',
         f'jest szkodnikiem, bo zjada ogórki',
         f'nie ma pleców',
         f'ty dzbanie',
         f'zgasiłabym Cię, ale gówno się nie pali :/',
-        f'ale Ci jedzie z halki xD',
-        f'jedzie Ci z pyska, jak z dupy tygryska :3'
+        f'ale Ci jedzie z halki',
+        f'jedzie Ci z pyska, jak z dupy tygryska'
     ]
 
     # append disses with user mention or user nickname
