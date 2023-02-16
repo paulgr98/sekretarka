c12fcb812e3032c7fec30fff569259bcc2ead0cd
paulgr98 <pawelwnuk98@gmail.com>
2023-02-17 00:10:00 +0100

    Changed converter.py to be compatible with Python 3.9

--- commands/converter.py
+++ commands/converter.py
@@ -2,18 +2,18 @@ from components import converters as conv
 
 
 def convert(method: str, text: str) -> str:
-    match method:
-        case 's2b':
-            return conv.str_to_binary(text)
-        case 'b2s':
-            return conv.binary_to_str(text)
-        case 's2h':
-            return conv.str_to_hex(text)
-        case 'h2s':
-            return conv.hex_to_str(text)
-        case 's2b64':
-            return conv.str_to_base64(text)
-        case 'b642s':
-            return conv.base64_to_str(text)
-        case _:
-            return 'Niepoprawna metoda konwersji. Możliwe wartości: s2b, b2s, s2h, h2s, s2b64, b642s'
+    if method == 's2b':
+        return conv.str_to_binary(text)
+    elif method == 'b2s':
+        return conv.binary_to_str(text)
+    elif method == 's2h':
+        return conv.str_to_hex(text)
+    elif method == 'h2s':
+        return conv.hex_to_str(text)
+    elif method == 's2b64':
+        return conv.str_to_base64(text)
+    elif method == 'b642s':
+        return conv.base64_to_str(text)
+    else:
+        return 'Niepoprawna metoda konwersji. Możliwe wartości: s2b, b2s, s2h, h2s, s2b64, b642s'
+
