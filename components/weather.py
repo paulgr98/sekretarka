787f852d43fdab6fc82aeae73be7df4d33bd3684
paulgr98 <pawelwnuk98@gmail.com>
2023-02-18 20:44:11 +0100

    Removed old code

--- components/weather.py
+++ components/weather.py
@@ -1,6 +1,7 @@
 import requests
 import config
 
+
 def get_location_key(city):
     url = 'http://dataservice.accuweather.com/locations/v1/cities/search'
 
@@ -22,20 +23,8 @@ def get_location_key(city):
     location_key = location['Key']
     return location_key
 
+
 def get_current_weather(city):
-    # OLD FOR OPEN WEATHER MAP
-    # url = "https://api.openweathermap.org/data/2.5/weather"
-    # url = 'https://community-open-weather-map.p.rapidapi.com/weather'
-    # querystring = {"q": city, "units": "metric", "lang": "pl"}
-
-    # headers = {
-    #     'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com",
-    #     'x-rapidapi-key': config.RAPID_API_KEY
-    # }
-    # response = requests.request("GET", url, headers=headers, params=querystring)
-    # return response.json()
-
-    # NEW FOR ACCUWEATHER
     location_key = get_location_key(city)
 
     current_conditions_url = f'http://dataservice.accuweather.com/currentconditions/v1/{location_key}'
@@ -53,18 +42,6 @@ def get_current_weather(city):
 
 
 def get_15_day_forecast(city):
-    # OLD FOR OPEN WEATHER MAP
-    # url = "https://community-open-weather-map.p.rapidapi.com/forecast/daily"
-    # querystring = {"q": city, "cnt": days, "units": "metric", "lang": "pl"}
-    #
-    # headers = {
-    #     'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com",
-    #     'x-rapidapi-key': config.RAPID_API_KEY
-    # }
-    # response = requests.request("GET", url, headers=headers, params=querystring)
-    # return response.json()
-
-    # NEW FOR ACCUWEATHER
     location_key = get_location_key(city)
 
     forecast_url = f'http://dataservice.accuweather.com/forecasts/v1/daily/5day/{location_key}'
@@ -80,4 +57,3 @@ def get_15_day_forecast(city):
     forecast_response.raise_for_status()
     forecast = forecast_response.json()
     return forecast
-
