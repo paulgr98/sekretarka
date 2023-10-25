cbe409e85c22b0703d029be718c07a5698c229f5
paulgr98 <pawelwnuk98@gmail.com>
2023-10-25 22:42:15 +0200

    Fixed rdt command

--- components/reddit.py
+++ components/reddit.py
@@ -11,7 +11,7 @@ class SubredditOver18(Exception):
     pass
 
 
-async def get_subreddit_random_hot(subreddit, user, is_nsfw_channel, limit):
+async def get_subreddit_random_hot(subreddit, is_nsfw_channel, limit):
     async with asyncpraw.Reddit(client_id=cfg.CLIENT_ID,
                                 client_secret=cfg.CLIENT_SECRET,
                                 user_agent=cfg.USER_AGENT) as reddit:
@@ -23,8 +23,9 @@ async def get_subreddit_random_hot(subreddit, user, is_nsfw_channel, limit):
             await sub.load()
 
             # check if subreddit is over 18 and the user doesn't have the mod permissions
-            if sub.over18 and (user.name != 'panpajonk' or not is_nsfw_channel):
-                raise SubredditOver18('O Ty zboczuszku! :3 Ten subreddit jest NSFW!')
+            if sub.over18:
+                if not is_nsfw_channel:
+                    raise SubredditOver18('O Ty zboczuszku! :3 Ten subreddit jest NSFW!')
 
             # if the limit is above 100, get the limit number of hot posts
             if limit > 50:
