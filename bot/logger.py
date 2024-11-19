5c451b662ad389e46c9bd55a34a65eca751d3932
paulgr98 <pawelwnuk98@gmail.com>
2024-11-19 01:24:58 +0100

    Revert logger changes

--- bot/logger.py
+++ bot/logger.py
@@ -1,26 +1,16 @@
 import logging
 
-import colorlog
-
-
 class BotLogger(object):
     def __init__(self):
-        self.handler = colorlog.StreamHandler()
-        self.logger = colorlog.getLogger('sekretarka')
-        self.logger.setLevel(logging.DEBUG)
-        self.logger.addHandler(self.handler)
+        self.handler = logging.StreamHandler()
+        self.logger = logging.getLogger('discord')
 
-    def info(self, message):
-        self.logger.info(message)
-
-    def warning(self, message):
-        self.logger.warning(message)
 
     def error(self, error):
         self.logger.error(f'{error} ({error.__class__.__name__})')
 
-    def critical(self, error):
-        self.logger.critical(f'{error} ({error.__class__.__name__})')
+    def info(self, message):
+        self.logger.info(message)
 
 
 logger = BotLogger()
